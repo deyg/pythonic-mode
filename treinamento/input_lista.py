58b884d15758d6b3e487bee840af7fd042231915
Denilson Barreto de Souza <denilson.souza@live.com>
2022-12-27 03:21:26 -0300

    refact: separacao de responsabilidades

--- treinamento/input_lista.py
+++ treinamento/input_lista.py
@@ -1,14 +1,27 @@
 clientes = []
+cliente = "_"
 
 
 def adiciona_cliente(nome):
-    clientes.append(nome)
+        clientes.append(nome)
 
 
-cliente = "_"
-while cliente != "":
-    cliente = input("Digite o nome do cliente: ")
+def cliente_valido(cliente):
     if cliente != "":
-        adiciona_cliente(cliente)
+        return True
+
+
+def apresenta_clientes(clientes):
         print(clientes)
-        
+
+
+def entrada_clientes():
+    global cliente
+    while cliente != "":
+        cliente = input("Digite o nome do cliente: ")
+        if cliente_valido(cliente):
+            adiciona_cliente(cliente)
+            apresenta_clientes(clientes)
+
+
+entrada_clientes()

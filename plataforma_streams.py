82459abb9ab0cfc21c3628798ea9542dbf7ec871
Denilson Barreto de Souza <denilson.souza@live.com>
2022-12-27 02:44:10 -0300

    refact: validacao de plano movida para metodo validador

--- plataforma_streams.py
+++ plataforma_streams.py
@@ -2,8 +2,11 @@ class Cliente:
     def __init__(self, nome, email, plano):
         self.nome = nome
         self.email = email
+        self.plano = self.plano_valido(plano)
+
+    def plano_valido(self, plano):
         self.lista_planos = ["basic", "premium"]
         if plano in self.lista_planos:
-            self.plano = plano
+            return plano
         else:
             raise Exception("Plano Invalido")
